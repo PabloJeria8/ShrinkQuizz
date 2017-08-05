8dbd43de52a0bb48464f90b29524e3007a4c1357
PabloJeria <pablo.jeria8@gmail.com>
2017-08-04 20:49:01 -0400

    first commit

---
+++ app/src/main/java/cl/empresapjm/shrinkquizz/PartyAnswer.java
@@ -0,0 +1,19 @@
+package cl.empresapjm.shrinkquizz;
+
+
+public class PartyAnswer {
+
+    public String answer;
+
+    public PartyAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+
+
+
+}
